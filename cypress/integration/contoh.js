391640c292f4344bf35b0c226d68c27698240db0
Farhan M R A <48043106+farhanmra@users.noreply.github.com>
2021-09-01 21:17:49 +0700

    update login

--- cypress/integration/contoh.js
+++ cypress/integration/contoh.js
@@ -1,5 +1,16 @@
-describe('',function(){
-    it('',function(){
+describe('coba',function(){
+    it('coba Login Gagal',function(){
         cy.visit('https://admin.pkh.dojobox.id')
+        cy.get('#input-email').type('farhan')
+        cy.get('#input-password').type('farhan')
+        cy.get('#btn-login').click()
+        cy.contains('Nama Pengguna atau Kata Sandi Anda tidak cocok').should('exist')
+    })
+    it('coba Login berhasil',function(){
+        cy.visit('https://admin.pkh.dojobox.id')
+        cy.get('#input-email').type('e@dojobox.id')
+        cy.get('#input-password').type('admin')
+        cy.get('#btn-login').click()
+        cy.contains('Super Admin').should('exist')
     })
 })
